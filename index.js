c997bf698c8691db9e45d96d23696814df7b3ad4
mohitlenka <mohitlenka865@gmail.com>
2022-10-22 08:28:52 +0530

    version1.0.0

---
+++ index.js
@@ -0,0 +1,12 @@
+let obj = document.querySelector('.pos');
+obj.addEventListener('click', (e)=>{
+    console.log(e)
+    let elem = document.querySelector('._header');
+    let classes = elem.classList
+    if(elem.style.display != 'none'){
+        elem.style.display = "none";
+    }
+    else{
+        elem.style.display = "block";
+    }
+});
